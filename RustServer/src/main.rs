c421361a7a0d58c31dca529c8e585fbe4787b2b0
KBluePurple <42506968+blwon@users.noreply.github.com>
2023-12-26 20:52:14 +0900

    예외 처리 추가

--- RustServer/src/main.rs
+++ RustServer/src/main.rs
@@ -1,3 +1,4 @@
+use std::net::IpAddr;
 use crossbeam_channel::unbounded;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::tcp::OwnedWriteHalf;
@@ -28,11 +29,19 @@ impl Packet {
 
         let string = String::from_utf8_lossy(buf);
 
-        let mut iter = string.split("|");
+        let iter: Vec<&str> = string.split("|").collect();
 
-        user_id.push_str(iter.next().unwrap());
-        time_stamp.push_str(iter.next().unwrap());
-        message.push_str(iter.next().unwrap());
+        if iter.len() != 3 {
+            return Self {
+                user_id: String::new(),
+                time_stamp: String::new(),
+                message: String::new(),
+            };
+        }
+
+        user_id.push_str(iter[0]);
+        time_stamp.push_str(iter[1]);
+        message.push_str(iter[2]);
 
         Self {
             user_id,
@@ -44,7 +53,11 @@ impl Packet {
 
 #[tokio::main]
 async fn main() {
-    let tcp = TcpListener::bind("127.0.0.1:4444").await.unwrap();
+    let tcp = match TcpListener::bind("127.0.0.1:4444").await {
+        Ok(tcp) => tcp,
+        Err(e) => panic!("Failed to bind to port 4444: {}", e),
+    };
+
     println!("Listening on port 4444");
 
     let mut writers: Vec<OwnedWriteHalf> = Vec::new();
@@ -54,9 +67,16 @@ async fn main() {
 
     tokio::spawn(async move {
         loop {
-            let (socket, _) = tcp.accept().await.unwrap();
+            let socket = match tcp.accept().await {
+                Ok((socket, _)) => socket,
+                Err(e) => panic!("Failed to accept socket: {}", e),
+            };
+
             println!("New client connected");
-            socket_sender.send(socket).unwrap();
+
+            if let Err(e) = socket_sender.send(socket) {
+                println!("Failed to send socket: {}", e);
+            }
         }
     });
 
@@ -69,19 +89,34 @@ async fn main() {
             tokio::spawn(async move {
                 let mut buf = [0; 1024];
                 loop {
-                    let n = reader.read(&mut buf).await.unwrap();
+                    let n = match reader.read(&mut buf).await {
+                        Ok(n) if n == 0 => {
+                            println!("Client disconnected");
+                            return;
+                        }
+                        Ok(n) => n,
+                        Err(e) => {
+                            println!("Failed to read from socket: {}", e);
+                            return;
+                        }
+                    };
                     if n == 0 {
                         return;
                     }
                     let packet = Packet::deserialize(&buf);
-                    sender.send(packet).unwrap();
+
+                    if let Err(e) = sender.send(packet) {
+                        println!("Failed to send packet: {}", e);
+                    }
                 }
             });
-        } 
+        }
 
         if let Ok(packet) = receiver.try_recv() {
             for writer in writers.iter_mut() {
-                writer.write_all(&packet.serialize()).await.unwrap();
+                if let Err(e) = writer.write_all(&packet.serialize()).await {
+                    println!("Failed to write to socket: {}", e);
+                }
             }
         }
     }
