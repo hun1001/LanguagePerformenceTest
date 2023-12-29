395ce381c5f92bf6bfc84271490e0e4282f24f4b
hun1001 <l01022899266@gmail.com>
2023-12-29 12:25:14 +0900

    기존 2개였던 서버에서 서버 하나 삭제

--- CSharpServer/CSharpMemoryPackServer/Program.cs
+++ CSharpServer/CSharpMemoryPackServer/Program.cs
@@ -1,7 +1,5 @@
 ﻿MemoryPackChatServer.TcpChatServer memoryServer = new MemoryPackChatServer.TcpChatServer();
-CustomPacket.TcpChatServer customServer = new CustomPacket.TcpChatServer();
 
 memoryServer.Run();
-customServer.Run();
 
 Console.ReadKey();
