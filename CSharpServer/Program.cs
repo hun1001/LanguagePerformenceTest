31a0473552c56fe91955ea71bd8dd772891fb45d
hun1001 <l01022899266@gmail.com>
2023-12-29 12:21:00 +0900

    C# 서버 분리

--- CSharpServer/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿MemoryPackChatServer.TcpChatServer memoryServer = new MemoryPackChatServer.TcpChatServer();
-CustomPacket.TcpChatServer customServer = new CustomPacket.TcpChatServer();
-
-memoryServer.Run();
-customServer.Run();
-
-Console.ReadKey();
--- Client/Assets/Scripts/Network/ServerType.cs
+++
@@ -1,7 +0,0 @@
-public enum ServerType : int
-{
-    CSMemoryPack = 1111,
-    CSCustom = 2222,
-    Cpp = 3333,
-    Rust = 4444,
-}
